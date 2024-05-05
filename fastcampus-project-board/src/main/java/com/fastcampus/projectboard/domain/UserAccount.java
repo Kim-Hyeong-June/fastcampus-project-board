44ee599d3d0b17eb85712afdb1a6d1c6d17726d7
Kim-Hyeong-June <york0095@naver.com>
2024-05-05 17:46:55 +0900

    #16 - 도메인 추가 작업
    user-account 도메인 만들고 jpaRepository 생성 , data.sql 에 유저 정보 입력

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -0,0 +1,66 @@
+package com.fastcampus.projectboard.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class UserAccount extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false , length = 50)
+    private String userId;
+
+    @Setter @Column(nullable = false)
+    private String userPassword;
+
+    @Setter @Column(length = 100)
+    private String email;
+
+    @Setter @Column(length = 100)
+    private String nickname;
+
+    @Setter private String memo;
+
+    protected UserAccount() {
+
+    }
+
+    public UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccount(userId, userPassword, email, nickname, memo);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof UserAccount that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
