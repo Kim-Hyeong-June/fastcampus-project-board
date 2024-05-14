516a03fe117dac06c4a58b16496ffcc2fb47cde5
Kim-Hyeong-June <york0095@naver.com>
2024-05-14 17:45:44 +0900

    #19 - 게시판 컨트롤러 구현

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentsResponse.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentsResponse.java
@@ -8,24 +8,24 @@ import java.time.LocalDateTime;
 /**
  * DTO for {@link com.fastcampus.projectboard.domain.ArticleComment}
  */
-public record ArticleCommentResponse(
+public record ArticleCommentsResponse(
         Long id,
         String content,
         LocalDateTime createdAt,
         String email ,
         String nickname
 ) implements Serializable {
-    public static ArticleCommentResponse of (Long id, String content, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    public static ArticleCommentsResponse of (Long id, String content, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleCommentsResponse(id, content, createdAt, email, nickname);
     }
 
-    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+    public static ArticleCommentsResponse from(ArticleCommentDto dto) {
         String nickname = dto.userAccountDto().nickname();
         if (nickname == null || nickname.isBlank()){
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return new ArticleCommentsResponse(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
