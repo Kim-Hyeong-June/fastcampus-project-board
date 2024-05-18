3ce54b273ba5f3a5168a5036e20a4770af6b8f61
Kim-Hyeong-June <york0095@naver.com>
2024-05-18 15:48:09 +0900

    #24 - 페이지 네이션 기능을 컨트롤러에 반영

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.response.ArticleResponse;
 import com.fastcampus.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.fastcampus.projectboard.service.ArticleService;
+import com.fastcampus.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,12 +17,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(@RequestParam(required = false)
@@ -31,12 +37,15 @@ public class ArticleController {
                                    direction = Sort.Direction.DESC) Pageable pageable,
                            ModelMap map)
     {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable)
-                .map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
+
         return "articles/index";
     }
-
-
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
@@ -44,6 +53,7 @@ public class ArticleController {
 
         map.addAttribute("article", article); // TODO : 구현 할때 실제 테이터를 넣어야한다.
         map.addAttribute("articleComments" , article.articleCommentResponses());
+        map.addAttribute("totalCount", articleService.getArticleCount());
 
         return "articles/detail";
     }
